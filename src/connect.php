78aab21a172edf436a15889b9a2e6659e18bd706
Nada Kourkmas <nadak8@hotmail.com>
2019-03-29 01:01:39 -0700

    changed db connection

--- src/connect.php
+++ src/connect.php
@@ -16,7 +16,7 @@
 	echo "Connected Successfully"; CloseCon($conn);
 	*/
 
-	$dbhost = "localhost";
-	$dbuser = "root"; $dbpass = "travel"; $db = "travel_destinations";
+	$dbhost = "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+	$dbuser = "t0iox4gdaafxsdul	"; $dbpass = "bclca4gzl99cywrz"; $db = "jdokw586fsalpqo5";
 	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 ?>
